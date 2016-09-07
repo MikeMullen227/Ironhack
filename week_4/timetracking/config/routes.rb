75c9e56d9c321922df922cf972ecf10cf971d1a3
Mike Mullen <mmullen227@gmail.com>
2016-09-07 10:39:05 -0400

    word central and first migrations to rails database

--- week_4/timetracking/config/routes.rb
+++ week_4/timetracking/config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
 	get '/contact', to: 'site#contact'
 
+	get 'say_name/:name', to: 'site#say_name'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
