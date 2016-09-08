bf40635f0d150c504f25441c7f3325115f29eaef
Mike Mullen <mmullen227@gmail.com>
2016-09-08 11:16:22 -0400

    timetracking defined create

--- week_4/timetracking/config/routes.rb
+++ week_4/timetracking/config/routes.rb
@@ -2,13 +2,19 @@ Rails.application.routes.draw do
  
 	get '/', to: 'site#home'
 
-	get '/projects', to: 'projects#index'
 
 	get '/contact', to: 'site#contact'
 
+	get 'say_name/:name', to: 'site#say_name'
+	
+	post '/projects', to: 'projects#create'
+	
+	get '/projects', to: 'projects#index'
+	
+	get '/projects/new', to: 'projects#new'
+
 	get '/projects/:index', to: 'projects#show'
 
-	get 'say_name/:name', to: 'site#say_name'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
