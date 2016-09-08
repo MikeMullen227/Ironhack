12ae89b8924d7eb099aa5f13d1ad83b690c620a5
Mike Mullen <mmullen227@gmail.com>
2016-09-08 12:26:00 -0400

    created new seeds with time entries

--- week_4/timetracking/db/seeds.rb
+++ week_4/timetracking/db/seeds.rb
@@ -6,22 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Project.create!(name: 'Ironhack', description: 'Ironhack is a...')
+iron_hack = Project.create!(name: 'Ironhack', description: 'Ironhack is a...')
 
-Project.create!(name: 'Time tracking app')
+recipes = Project.create!(name: 'Recipes', description: 'Track my favorite recipes' )
 
-Project.create!(name: 'Recipes', description: 'Track my favorite recipes')
+movies = Project.create!(name: 'Movies', description: 'Track my favorite movies')
 
-Project.create!(name: 'Movies', description: 'Track my favorite movies')
+music = Project.create!(name: 'Music', description: 'Track my favorite music')
 
-Project.create!(name: 'Music', description: 'Track my favorite music')
+hobbies = Project.create!(name: 'Hobbies', description: 'Track my favorite hobbies')
 
-Project.create!(name: 'Hobbies', description: 'Track my favorite hobbies')
+food = Project.create!(name: 'Food', description: 'Track my favorite food')
 
-Project.create!(name: 'Food', description: 'Track my favorite food')
+cars = Project.create!(name: 'Cars', description: 'Track my favorite cars')
 
-Project.create!(name: 'Cars', description: 'Track my favorite cars')
+animals = Project.create!(name: 'Animals', description: 'Track my favorite animals')
 
-Project.create!(name: 'Animals', description: 'Track my favorite animals')
+destinations = Project.create!(name: 'Destinations', description: 'Track my favorite destinations')
 
-Project.create!(name: 'Destinations', description: 'Track my favorite destinations')
+
+iron_hack.time_entries.create(hours: '0', minutes: '50')
+
+recipes.time_entries.create(hours: '0', minutes: '35')
+
+
+movies.time_entries.create(hours: '1', minutes: '20')
