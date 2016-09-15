16b9f1bb6e0dc3084f18c2f14c3097c27db163e5
Mike Mullen <mmullen227@gmail.com>
2016-09-15 18:33:08 -0400

    spotify iteration 2

--- week_5/day_23/spotify/app.js
+++ week_5/day_23/spotify/app.js
@@ -20,27 +20,11 @@ $.ajax({
 	error: handleError
 });
 
-}
-
-function albumList(event){
-	
 
 
-	
-$.ajax({
-	type: "Get",
-	url: `https://api.spotify.com/v1/search?type=artist&query=${search_term}`,
-	success: showAlbum,
-	error: handleError
-
-});
 
 }
 
-
-function showAlbum(response){
-
-}
 
 function showArtist(response){
 
@@ -48,16 +32,19 @@ function showArtist(response){
 	
 	console.log(artistArray)
 	 // $(".js-artist-list").empty();
-	artistArray.forEach(function(theArtist, images){
+	artistArray.forEach(function(theArtist){
 		if (theArtist.images[0]){
 
 		var html = `
 		<li>
 			<h2> ${theArtist.name} </h2>
+			<img class="js-confusion" data-lost="${theArtist.id}" src="${theArtist.images[0].url}">
+			
 		</li>
 		<li>
-			<img src="${theArtist.images[0].url}">
+			<button class="js-album-search" data-lost="${theArtist.id}" src="${theArtist.name}"> some text</button>
 		</li>	
+		<ul class="js-album-list"></ul>
 		`;
 		$('.js-artist-list').append(html);
 		
@@ -65,8 +52,43 @@ function showArtist(response){
 	 
 		// if (theArtist.images[0].url) = null
 	});
+
+	$(".js-album-search").on("click", albumSearch);
+
+	function albumSearch(theEvent){
+		var artistId = $(theEvent.currentTarget).data("lost");
+
+
+$.ajax({
+	type: "Get",
+	url: `https://api.spotify.com/v1/artists/${artistId}/albums`,
+	success: showAlbum,
+	error: handleError
+
+});
+
+}
 		
 
+}
+
+function showAlbum(response){
+	var albumsResults = response.items;
+	console.log(albumsResults);
+
+
+	albumsResults.forEach(function(theAlbum){
+		if (theAlbum.images[0]){
+			var albumHtml = `
+			<li>
+				<h2> ${theAlbum.name} </h2>
+				<img src=${theAlbum.images[0].url}
+			</li>
+		`;
+		$('.js-album-list').append(albumHtml);
+		
+		}
+		});
 	}
 
 
